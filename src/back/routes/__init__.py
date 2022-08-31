0639aa125cb86f2aa2b7b885525730a4c600bc9c
Marat <maratabdrahmanov@yandex.ru>
2022-08-31 10:51:34 +0300

    Разработан Endpoint для обращения к остальным микросервисам

--- src/back/routes/__init__.py
+++ src/back/routes/__init__.py
@@ -1,6 +1,10 @@
+import json
+
 import requests
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from urllib.parse import urljoin
+
+from pydantic import ValidationError
 
 from back.models.models import (NodalCalcResponse, VlpIprCalcResponse,
                                 WellModelCalcRequest, WellModelCalcResponse)
@@ -12,6 +16,26 @@ from back.config import Settings
 main_router = APIRouter(prefix="/well_model", tags=["WellModel"])
 
 
+def check_correct_data(data: WellModelCalcRequest):
+    pass
+def general_requests(data: WellModelCalcRequest):
+    try:
+        vlp_data = form_vlp_request(data)
+    except ValidationError:
+        return False
+    vlp = requests.post(url="http://localhost:8001/vlp/calc", json=vlp_data)
+    ipr_data = form_ipr_request(data)
+    ipr = requests.post(url="http://localhost:8002/ipr/calc", json=ipr_data)
+    nodal_data = form_nodal_request(vlp.json(), ipr.json())
+    nodal = requests.post(url="http://localhost:8003/nodal/calc", json=nodal_data)
+    return {"vlp": vlp, "ipr": ipr, "nodal": nodal}
+
+
 @main_router.put("/calc", response_model=WellModelCalcResponse)
-async def my_profile(data: WellModelCalcRequest):
-    return
+def my_profile(data: WellModelCalcRequest):
+
+    former_data = general_requests(data)
+    if former_data == False:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return WellModelCalcResponse(ipr=former_data["ipr"].json(), vlp=former_data["vlp"].json(),
+                                 nodal=former_data["nodal"].json())
--- src/back/routes/request_formers.py
+++ src/back/routes/request_formers.py
@@ -1,14 +1,27 @@
 from back.models.models import (PVT, IprCalcRequest, NodalCalcRequest,
-                                VlpCalcRequest)
+                                VlpCalcRequest, WellModelCalcRequest, VlpIprCalcResponse)
 
 
-def form_vlp_request(well_model_request):
-    return
+def form_vlp_request(well_model_request: WellModelCalcRequest):
+    inclinometry = well_model_request.inclinometry
+    casing = well_model_request.casing
+    tubing = well_model_request.tubing
+    pvt = well_model_request.pvt
+    p_wh = well_model_request.p_wh
+    geo_grad = well_model_request.geo_grad
+    h_res = well_model_request.h_res
+    return VlpCalcRequest(inclinometry=inclinometry, casing=casing,
+                          tubing=tubing, pvt=pvt, p_wh=p_wh, geo_grad=geo_grad,
+                          h_res=h_res).dict()
 
 
-def form_ipr_request(well_model_request):
-    return
+def form_ipr_request(well_model_request:WellModelCalcRequest):
+    p_res = well_model_request.p_res
+    wct = well_model_request.pvt.wct
+    pi = well_model_request.pi
+    pb = well_model_request.pvt.pb
+    return IprCalcRequest(p_res=p_res, wct=wct, pi=pi, pb=pb).dict()
 
 
 def form_nodal_request(vlp_result, ipr_result):
-    return
+    return NodalCalcRequest(vlp=vlp_result, ipr=ipr_result).dict()
